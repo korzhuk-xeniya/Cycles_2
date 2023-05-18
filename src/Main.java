7ba8c6f62dbee6caf2e2dcbbd064d66a4e83d8a9
Ксения <korzhuk.xeniya@yandex.ru>
2023-05-18 23:12:00 +0300

    task_1

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int monthlyContibution = 15000;
+        int total = 0;
+        int i = 0;
+        while (total < 2_459_000) {
+            total = total + monthlyContibution;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+
+
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+
+    }
+
+}
